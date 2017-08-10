7c280daa84bc63c922e83ec023134d20ac5afca9
Brendan Sullivan <sullivanbt@ornl.gov>
2017-08-10 18:06:21 -0400

    Removed 1/q version of ICConvoluted'

--- ICConvoluted.py
+++ ICConvoluted.py
@@ -21,17 +21,7 @@ class IkedaCarpenterConvoluted(IFunction1D):
                 self.declareParameter("hatWidth") #c[5]
                 self.declareParameter("k_conv") #c[6]
 		self.declareParameter("bg") #c[7]
-		'''
-                self.declareParameter("Alpha") #c[0]//C1
-                self.declareParameter("Beta") #c[1]//C2
-                self.declareParameter("R") #c[2]//C3
-                self.declareParameter("Ta") #c[3]//C4
-                self.declareParameter("Tb") #c[4]//C5
-                self.declareParameter("Tc") #c[5]//C6
-                self.declareParameter("Td") #c[6]//C7
-                self.declareParameter("gc_alpha") #c[7]//C8
-                self.declareParameter("pp_width") #c[8]//C9
-		'''
+	
 	#Evaluate the function
 	def function1D(self, t):
 		A  = self.getParamValue(0) 
@@ -73,51 +63,8 @@ class IkedaCarpenterConvoluted(IFunction1D):
 		
 
  		return f_int
-        ''' #This is the original version, not related to the actual xscale
-	def function1D(self, t_prim_n):
-		singleFlag = False
-		if len(t_prim_n) == 1:
-			t_prim_n = np.array(range(20))+1
-			singleFlag = True
-                c = np.zeros(self.numParams())
-                for i in range(self.numParams()):
-                        c[i] = self.getParamValue(i)
-                n_time = 20#np.prod(np.shape(t_prim_n))
-                t_prim = np.array(range(n_time))+1
-                t_prim = c[5]*(t_prim-c[3])/(c[4]-c[3])
-                f_int = (1-c[2])*(c[0]/2)*\
-                        (np.power(c[0]*t_prim,2))*np.exp(-c[0]*t_prim)+\
-                        (c[2]*c[1]*np.power(c[0],3)/np.power(c[0]-c[1],3))*\
-                        (np.exp(-c[1]*t_prim)-np.exp(-c[0]*t_prim)*(1+(c[0]-c[1])*\
-                        t_prim+0.5*np.power((c[0]-c[1])*t_prim,2)))
-                f_int[t_prim<0] = 0
-                mid_point_hat = len(f_int)//2
-                gc_x = np.array(range(len(f_int))).astype(float)
-                ppd = 0.0*gc_x
-                lowIDX  = np.max([mid_point_hat-np.abs(c[8]),0])
-                highIDX = np.min([mid_point_hat+np.abs(c[8]),len(gc_x)])
-                
-		#print c, lowIDX, highIDX
-		ppd[lowIDX:highIDX] = 1.0;
-		ppd = ppd/sum(ppd);
-
-                gc_x = np.array(range(len(f_int))).astype(float)
-                gc_x = 2*(gc_x-np.min(gc_x))/(np.max(gc_x)-np.min(gc_x))-1;
-                gc_f = np.exp(-c[7]*np.power(gc_x,2));
-                gc_f = gc_f/np.sum(gc_f);
-
-                #For compatability with Rick's code, we'll have to do a full
-                # convolution and select the elements we want.
-                npad = len(f_int) - 1
-                first = npad - npad//2
-                f_int = np.convolve(f_int,ppd,'full')[first:first+len(f_int)];
-                f_int = np.convolve(f_int,gc_f,'full')[first:first+len(f_int)];
-                f_int = f_int+c[6];
-                if singleFlag:
-			return np.array(np.max(f_int))
-		return f_int
-	'''
-	#Evaluate the function for a differnt set of paremeters (trialc)
+      
+ 	#Evaluate the function for a differnt set of paremeters (trialc)
 	def function1DDiffParams(self, xvals, trialc):
 		#First, grab the original parameters and set to trialc
                 c = np.zeros(self.numParams())
