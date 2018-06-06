88e591620d514526796867945d46b078c57ff0ab
Brendan Sullivan <sullivanbt@ornl.gov>
2018-06-06 09:15:46 -0400

    flake8 mbvg

--- mbvg.py
+++ mbvg.py
@@ -1,11 +1,10 @@
-import math
 import numpy as np
 from mantid.api._api import IFunction1D
 from matplotlib.mlab import bivariate_normal
 
 
 class MBVG(IFunction1D): #MantidBVG
-    '''
+    """
     MBVG implements a bivariate gaussian (BVG) in Mantid (M) as a 1D function.  This is done so that it can be
     fit in a straightforward fashion using Mantid's Fit() function.  To achieve this, we use the flattened
     version of the 2D profile and fit it as a 1D function.  It is built on matplotlib.mlab.bivariate_normal, which
@@ -39,7 +38,7 @@ class MBVG(IFunction1D): #MantidBVG
         fitResults = Fit(Function=m, InputWorkspace='bvgWS', Output='bvgfit')
 
         BS - May 18 2018
-    '''
+    """
     def init(self):
         self.declareParameter("A") #Amplitude
         self.declareParameter("muX") #Mean along the x direction
@@ -54,19 +53,19 @@ class MBVG(IFunction1D): #MantidBVG
         self.addConstraints("0 < sigX") #standard deviations must be positive
         self.addConstraints("0 < sigY") #standard deviations must be positive
         self.addConstraints("0 < bg") #standard deviations must be positive
- 
+
     def function1D(self, t):
-        '''
+        """
         function1D returns the flattened version of the function.
         Input, t, may be in one of two forms:
-            1) a 1D array (e.g. pos.ravel() from the example).  
+            1) a 1D array (e.g. pos.ravel() from the example).
             2) a 3D array (e.g. pos from the example)
         Output
-            If input is of type 1, a 1D array matching the size of the 
+            If input is of type 1, a 1D array matching the size of the
             input is returned.  This allows fitting to take place.
-            If input is of type 2, a 1D array matching the size of 
+            If input is of type 2, a 1D array matching the size of
             pos.ravel() is returned.
-        '''
+        """
         if t.ndim == 1:
             nX = int(self.getAttributeValue('nX'))
             nY = int(self.getAttributeValue('nY'))
@@ -86,7 +85,7 @@ class MBVG(IFunction1D): #MantidBVG
 
         sigXY = sigX*sigY*sigP
         Z = A*bivariate_normal(X,Y, sigmax=sigX, sigmay=sigY,
-                            mux=muX,muy=muY,sigmaxy=sigXY)
+                               mux=muX,muy=muY,sigmaxy=sigXY)
         if t.ndim == 1:
             zRet = np.empty(Z.shape+(2,))
             zRet[:,:,0] = Z
@@ -111,14 +110,13 @@ class MBVG(IFunction1D): #MantidBVG
         return self.getMu(), self.getSigma()
 
     def setConstraints(self, boundsDict):
-        '''
+        """
         setConstraints sets fitting constraints for the mbvg function.
         Intput:
-            boundsDict: a dictionary object where each key is a parameter as a string 
-                        ('A', 'muX', 'muY', 'sigX', 'sigY', 'sigP', 'bg') and the value is 
+            boundsDict: a dictionary object where each key is a parameter as a string
+                        ('A', 'muX', 'muY', 'sigX', 'sigY', 'sigP', 'bg') and the value is
                         an array with the lower bound and upper bound
-
-        '''
+        """
         for param in boundsDict.keys():
             try:
                 if boundsDict[param][0] < boundsDict[param][1]:
@@ -128,10 +126,11 @@ class MBVG(IFunction1D): #MantidBVG
                     print 'Setting constraints on mbvg; reversing bounds'
                     self.addConstraints("{:4.4e} < A < {:4.4e}".format(boundsDict[param][1], boundsDict[param][0]))
             except ValueError:
-                print 'Cannot set parameter {:s} for mbvg.  Valid choices are (\'A\', \'muX\', \'muY\', \'sigX\', \'sigY\', \'sigP\', \'bg\')'.format(param)
+                print 'Cannot set parameter {:s} for mbvg.  Valid choices are', \
+                      '(\'A\', \'muX\', \'muY\', \'sigX\', \'sigY\', \'sigP\', \'bg\')'.format(param)
 
     def function2D(self, t):
-        '''
+        """
         function2D returns the 2D version of the BVG.
         Input may be in two forms:
             1) 1D array (e.g. pos.ravel()).  This will be reshaped into an nX*nY*2 array, so
@@ -140,7 +139,7 @@ class MBVG(IFunction1D): #MantidBVG
         Output:
             a 2D array either size nX*nY (intput type 1) or A*B (input type 2) with intensities
             of the BVG.
-        '''
+        """
         if t.ndim == 1:
             nX = int(self.getAttributeValue('nX'))
             nY = int(self.getAttributeValue('nY'))
@@ -159,9 +158,9 @@ class MBVG(IFunction1D): #MantidBVG
 
         sigXY = sigX*sigY*sigP
         Z = A*bivariate_normal(X,Y, sigmax=sigX, sigmay=sigY,
-                            mux=muX,muy=muY,sigmaxy=sigXY)
+                               mux=muX,muy=muY,sigmaxy=sigXY)
         Z += bg
-        return Z        
+        return Z
 
     def function3D(self, t):
         if t.ndim == 4:
@@ -178,13 +177,11 @@ class MBVG(IFunction1D): #MantidBVG
 
         sigXY = sigX*sigY*sigP
         Z = A*bivariate_normal(X,Y, sigmax=sigX, sigmay=sigY,
-                            mux=muX,muy=muY,sigmaxy=sigXY)
+                               mux=muX,muy=muY,sigmaxy=sigXY)
         Z += bg
         return Z
 
-
-
-    #Evaluate the function for a differnt set of paremeters (trialc)
+    # Evaluate the function for a differnt set of paremeters (trialc)
     def function1DDiffParams(self, xvals, trialc):
         #First, grab the original parameters and set to trialc
         c = np.zeros(self.numParams())
@@ -200,23 +197,17 @@ class MBVG(IFunction1D): #MantidBVG
             self.setParameter(i, c[i])
         return f_trial
 
-
-    #Construction the Jacobian (df) for the function    
+    # Construction the Jacobian (df) for the function
     def functionDeriv1D(self, xvals, jacobian, eps=1.e-3):
         f_int = self.function1D(xvals)
         #Fetch parameters into array c
         c = np.zeros(self.numParams())
         for i in range(self.numParams()):
             c[i] = self.getParamValue(i)
-        c_shape = c.shape
         nc = np.prod(np.shape(c))
-        f_shape = f_int.shape
-        nf = np.prod(f_shape)
         for k in range(nc):
             dc = np.zeros(nc)
             dc[k] = max(eps,eps*c[k])
             f_new = self.function1DDiffParams(xvals,c+dc)
             for i,dF in enumerate(f_new-f_int):
                 jacobian.set(i,k,dF/dc[k])
-
-
